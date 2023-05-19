e679de13a7c3daf7a15625345fce2539c6064943
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-05-20 04:06:22 +0600

    Tabs are ready

--- src/pages/home/tabs/Tab.jsx
+++ src/pages/home/tabs/Tab.jsx
@@ -1,92 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Tab.css'
 
 const Tab = () => {
     const [currentTab, setCurrentTab] = useState('1');
-    const tabs = [
+    const [tabs, setTabs] = useState([]);
 
-        {
-            id: 1,
-            category: "Vintage_Car",
-            toys: [
-                {
-                    "picture": "sports_car1.jpg",
-                    "name": "Speedster GT",
-                    "price": "$29.99",
-                    "rating": 4.5,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-                },
-                {
-                    "picture": "sports_car2.jpg",
-                    "name": "Turbo Racer",
-                    "price": "$19.99",
-                    "rating": 4.2,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-
-                }
-            ]
-        },
-        {
-            id: 2,
-            category: "Off-Road_Vehicles",
-            toys: [
-                {
-                    "picture": "offroad_car1.jpg",
-                    "name": "Mud Runner",
-                    "price": "$24.99",
-                    "rating": 4.7,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-                },
-                {
-                    "picture": "offroad_car2.jpg",
-                    "name": "Trail Blazer",
-                    "price": "$32.99",
-                    "rating": 4.4,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-                }
-            ]
-        },
-
-        {
-            id: 3,
-            category: "Monster_Trucks",
-            toys: [
-                {
-                    "picture": "monster_truck1.jpg",
-                    "name": "Goliath",
-                    "price": "$34.99",
-                    "rating": 4.3,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-                },
-                {
-                    "picture": "monster_truck2.jpg",
-                    "name": "Bigfoot",
-                    "price": "$29.99",
-                    "rating": 4.1,
-                    "seller_name": "Toni Kroos",
-                    "seller_email": "tonibhai@gmail.com",
-                    "available_quantity": 20,
-                    "description": "Very good car. Bissho shera"
-                }
-            ]
-        },
-
-    ];
+    useEffect(() => {
+        fetch('http://localhost:5000/toys')
+            .then(res => res.json())
+            .then(data => setTabs(data.slice(0, 3)))
+    })
 
     const handleTabClick = (e) => {
         setCurrentTab(e.target.id);
@@ -96,13 +19,33 @@ const Tab = () => {
         <div className='container pb-32'>
             <div className='tabs lg:grid lg:grid-cols-3'>
                 {tabs.map((tab) =>
-                    <button id={tab.id} disabled={currentTab === `${tab.id}`} onClick={(handleTabClick)}>{tab.category}</button>
+                    <button id={tab.c_id} disabled={currentTab === `${tab.c_id}`} onClick={(handleTabClick)}>{tab.category}</button>
                 )}
             </div>
             <div className='content'>
                 {tabs.map((tab) =>
                     <div >
-                        {currentTab === `${tab.id}` && <div><p className='title'>{tab.category}</p><p>{tab.category}</p></div>}
+                        {currentTab === `${tab.c_id}` && <div>
+                            
+                            <div className='flex justify-between m-12'>
+                                {
+                                    tab.toysgula.map((toy) =>
+                                        <div className="card drop-shadow-2xl lg:card-side bg-[#1A1A1A] shadow-xl">
+                                            <figure><img className='h-40 w-full  lg:h-full lg:w-64' src={toy.picture} alt="Album" /></figure>
+                                            <div className="card-body">
+                                                <h2 className="font-semibold text-white">Name: {toy.name}</h2>
+                                                <p className='font-semibold text-white'>Price: {toy.price}</p>
+                                                <p className='font-semibold text-white'>Rating: {toy.rating}</p>
+                                                <div className="card-actions ">
+                                                    <button className="btn btn-primary">View Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+
+                        </div>}
                     </div>
                 )}
             </div>
