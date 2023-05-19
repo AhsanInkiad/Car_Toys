248b4761009467a7c5781bc3b6b793f42b03e212
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-05-19 14:58:31 +0600

    Registration process is ready. Now fixing log in process

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Register.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Register = () => {
+    const [err, setErr] = useState('');
+    const { createUser } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
     const handleRegister = event => {
-        
+
         event.preventDefault();
-        
+
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
@@ -14,10 +20,25 @@ const Register = () => {
         const photo = form.photo.value;
 
         console.log(name, email, password, photo);
+
+
+        createUser(email, password)
+            .then(result => {
+                const createdUser = result.user;
+                setErr('');
+                {/* Navigate to log in when registration is done */}
+                navigate('/login')
+                
+                event.target.reset();
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                setErr(errorMessage.slice(9,));
+            })
     }
     return (
         <div className='flex py-20'>
-            <form onSubmit={ handleRegister} className="Laform mx-auto">
+            <form onSubmit={handleRegister} className="Laform mx-auto">
                 <div className="Latitle mx-auto  text-center"> <span className=''></span>     Welcome<br /><span>Complete your registration :</span></div>
                 <input type="text" placeholder="Name" name="name" className="Lainput" required />
                 <input type="email" placeholder="Email" name="email" className="Lainput" required />
@@ -25,7 +46,7 @@ const Register = () => {
                 <input type="text" placeholder="Photo URL" name="photo" className="Lainput" required />
                 <button className='mx-auto w-1/2 btn btn-xs sm:btn-sm md:btn-md lg:btn-md btn-outline btn-warning '> <span className='text-black'>Register</span> </button>
 
-               
+
                 <div className="-mt-6 font-semibold text-[#666] mx-auto   text-center"> <br /><span className='text-sm'>Already have an account? : <Link to='/login'> <u className=' hover:text-blue-500 text-md font-bold'>Log in</u></Link> </span></div>
             </form>
 
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,41 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-const AuthProvider = ({ children }) => {
-    const authInfo = {
 
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signIn = (email, password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+         setUser(currentUser);
+         setLoading(false);
+         })
+ 
+         return () =>{
+             unsubscribe()
+         }
+     },[])
+   
+    const authInfo = {
+        user,
+        setUser,
+        loading,
+        createUser,
+        signIn
     }
 
     return (
