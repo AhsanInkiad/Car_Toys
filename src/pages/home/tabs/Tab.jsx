603cd58294829e5335ee0c96f62082c0df869140
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-05-20 04:14:42 +0600

    Ratings have been added in tabs element

--- src/pages/home/tabs/Tab.jsx
+++ src/pages/home/tabs/Tab.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { FaRegStar, FaStar } from "react-icons/fa";
+import Rating from 'react-rating';
 import './Tab.css'
 
 const Tab = () => {
@@ -26,7 +28,7 @@ const Tab = () => {
                 {tabs.map((tab) =>
                     <div >
                         {currentTab === `${tab.c_id}` && <div>
-                            
+
                             <div className='flex justify-between m-12'>
                                 {
                                     tab.toysgula.map((toy) =>
@@ -36,6 +38,13 @@ const Tab = () => {
                                                 <h2 className="font-semibold text-white">Name: {toy.name}</h2>
                                                 <p className='font-semibold text-white'>Price: {toy.price}</p>
                                                 <p className='font-semibold text-white'>Rating: {toy.rating}</p>
+                                                <Rating className='rate mb-3 text-[#EAB902]'
+                                                    placeholderRating={toy.rating}
+                                                    readonly
+                                                    emptySymbol={<FaRegStar></FaRegStar>}
+                                                    placeholderSymbol={<FaStar></FaStar>}
+                                                    fullSymbol={<FaStar></FaStar>}
+                                                ></Rating>
                                                 <div className="card-actions ">
                                                     <button className="btn btn-primary">View Details</button>
                                                 </div>
